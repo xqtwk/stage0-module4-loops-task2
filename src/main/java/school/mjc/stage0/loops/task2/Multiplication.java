1d20215c7b8088b9172f2d6e2ae10f6f7a5dca61
Richard <xqtwkk@gmail.com>
2022-07-26 00:29:33 +0300

    attempt 3, fixing multiplication

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,8 +3,14 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         if (multiplyByAndToInclusive != 0) {
-            for (int i = 0; i <= multiplyByAndToInclusive; i++) {
-                System.out.println(i * multiplyByAndToInclusive);
+            if (multiplyByAndToInclusive > 0) {
+                for (int i = 0; i <= multiplyByAndToInclusive; i++) {
+                    System.out.println(i * multiplyByAndToInclusive);
+                }
+            } else {
+                for (int i = 0; i >= multiplyByAndToInclusive; i--) {
+                    System.out.println(i * multiplyByAndToInclusive);
+                }
             }
         }
     }
